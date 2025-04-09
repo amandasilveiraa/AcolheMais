9ab73ac7fc7e60b1bb54e93d1d25e04f29027d3e
rafaelfranzon290905 <84185550+rafaelfranzon290905@users.noreply.github.com>
2025-04-09 14:05:17 -0300

    Add files via upload

--- src/pages/register/register.js
+++ src/pages/register/register.js
@@ -1,17 +1,35 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { Background} from '../../StyledGlobal.js';
-import {LinkStyle, Container, Esquerda, Direita} from './register.jsx';
+import {LinkStyle, Container, Esquerda, Direita, Logo, Titulo, Subtitulo} from './register.jsx';
+import imgLogo from '../../assets/LogoBrancaAcolheMais.svg'
+import { CadastroForm } from '../../components/CadastroForm/cadastroForm.js'
+import { useState } from 'react';
 
-export function Register () {
+export const Register = () => {
+    const [menu, setMenu] = useState();
+
+    function ExibirMenu(){
+        if(menu){
+            setMenu(false)
+        } else{
+            setMenu(true)
+        }
+    }
+    
     return (
+        
+
         <>
             <Container>
                 <Esquerda>
+                    <Logo src={imgLogo}></Logo>
+                    <Titulo>Crie sua conta</Titulo>
+                    <Subtitulo>Complete esses simples passos para se registrar</Subtitulo>
                     <LinkStyle to='/'>Vai pro home</LinkStyle>
                 </Esquerda>
                 <Direita>
-                    
+                    <CadastroForm></CadastroForm>
                 </Direita>
             </Container>
             
